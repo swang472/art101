8dcb9f0babc355601c616fe8b2008e588216caeb
swang472 <swang472@ucsc.edu>
2025-04-17 12:58:58 -0700

    Lab 4 work

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock paper scissor game
+// Author: Angela Wang
+// Date: 4/17/25
 
-// Constants
+//Accept user input 
+  //Has to be either rock, paper or scissors
+//Validate user input 
+  //Double check to see what the chose
+//Evaluate possible moves 
+  //Choose whether opponent will choose rock, paper or scissors
+//Choose move 
+  //Make selection of input (rock, paper, or scissors) 
+//Run win-test of result 
+  //See what both parties chose
 
-// Functions
+
+
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
