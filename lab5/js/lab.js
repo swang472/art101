5727e967f8195ffb843a731571af32143e4e6b27
swang472 <swang472@ucsc.edu>
2025-04-22 18:13:32 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,21 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Angela Wang
+// Date: 4/22/25
 
-// Constants
+make = "BMW";
+model = "Mini Cooper";
+color = "yellow";
+year = "2017";
+age = 2025-year;
 
-// Functions
+document.writeln ("make: " + make + "<br>");
+document.writeln ("model: " + model + "<br>");
+document.writeln ("color: " + color + "<br>");
+document.writeln ("year: "+ year + "<br>");
+document.writeln("age: " + age + " years<brs>");
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
