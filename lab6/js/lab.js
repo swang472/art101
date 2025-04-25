e8a833e94dca35d5fe107a2722595f08f5cd3a4a
swang472 <swang472@ucsc.edu>
2025-04-24 17:43:31 -0700

    lab 6 work

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6: Arrays and Objects
+// Author: Angela Wang
+// Date: 4/24/25
 
-// Constants
+myTransport = ["walking", " biking", " taking the bus", " driving"];
 
-// Functions
+myMainRide = {
+    make:"BMW",
+    model:"mini cooper",
+    color:"yellow",
+    year:2017,
+
+    age: function(){
+      return 2025 - this.year;
+    }
+}
+
+document.writeln("Getting around: "+ myTransport+ "<br>");
+document.writeln("myMainRide: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
