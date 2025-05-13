d582ead657373031e261898735cf616d436ab1d5
swang472 <swang472@ucsc.edu>
2025-05-12 20:07:52 -0700

    lab 9 work

---
+++ lab9/js/lab.js
@@ -0,0 +1,13 @@
+/*
+Angela Wang
+Lab 9: Libraries and JQuery
+5/11/2025
+*/
+$("#Challenge").append("<button id= 'button-challenge'>Make Speical</button>");
+$("#Problems").append("<button id='button challenge'>Make Special</button>");
+$("#Reflection").append("<button id='button challenge'>Make Special</button>");
+
+$("#button-challenge").click(function(){
+    $("#Cshallenge").toggleClass("special");
+});
+Main();
