e0763ffea6d93f59db2612e99654d4b1e6438bf0
swang472 <swang472@ucsc.edu>
2025-05-15 15:02:45 -0700

    lab10

---
+++ lab10/js/lab.js
@@ -0,0 +1,19 @@
+/*
+Angela Wang
+Lab 9: Libraries and JQuery
+5/11/2025
+*/
+function generateRandomText(){
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  return text.slice(randStart, randStart + randLen);
+}
+$("#make-convo").click(function(){
+  console.log("Click")
+  const newText = generateRandomText();
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
+
